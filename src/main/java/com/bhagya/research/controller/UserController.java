b1995a46e8b2050b44445efb6c286a7606857f1f
Bhagya Rathnayake <rathnayake.bhagya94@gmail.com>
2021-07-17 23:07:54 +0530

    Made changes to User entity

--- src/main/java/com/bhagya/research/controller/UserController.java
+++ src/main/java/com/bhagya/research/controller/UserController.java
@@ -2,7 +2,7 @@ package com.bhagya.research.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bhagya.research.dashboard.user.dto.AppUserDTO;
@@ -15,7 +15,7 @@ public class UserController {
 	AppUserDetailsService appUserService;
 	
 	@PostMapping("/saveUser")
-	public void saveUser(@RequestParam AppUserDTO appUserDTO) {
+	public void saveUser(@RequestBody AppUserDTO appUserDTO) {
 		appUserService.saveUser(appUserDTO);
 	}
 	
