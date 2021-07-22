31914e68892f5a2fdadab07fb837840d912d063a
Bhagya Rathnayake <rathnayake.bhagya94@gmail.com>
2021-07-22 21:40:26 +0530

    Auth response model updated

--- src/main/java/com/bhagya/research/core/model/AuthenticationResponse.java
+++ src/main/java/com/bhagya/research/core/model/AuthenticationResponse.java
@@ -1,13 +1,26 @@
 package com.bhagya.research.core.model;
 
+import com.bhagya.research.entity.enums.UserLevel;
+
 public class AuthenticationResponse {
 	private final String jwt;
+	private final String userName;
+	private final UserLevel userLevel;
 
-	public AuthenticationResponse(String jwt) {
+	public AuthenticationResponse(String jwt, String userName, UserLevel userLevel) {
 		this.jwt = jwt;
 	}
 
-	public String getJWT() {
+	public String getJwt() {
 		return jwt;
 	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public UserLevel getUserLevel() {
+		return userLevel;
+	}
+	
 }
