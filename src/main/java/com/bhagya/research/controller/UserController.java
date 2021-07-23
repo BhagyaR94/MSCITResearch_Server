ad3363db481caef322424e8494fdc60469fb00c2
Bhagya Rathnayake <rathnayake.bhagya94@gmail.com>
2021-07-23 18:33:19 +0530

    user enrolling and signup fixes

--- src/main/java/com/bhagya/research/controller/UserController.java
+++ src/main/java/com/bhagya/research/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.bhagya.research.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,7 @@ import com.bhagya.research.dashboard.user.dto.AppUserDTO;
 import com.bhagya.research.dashboard.user.service.AppUserDetailsService;
 
 @RestController
+@CrossOrigin(maxAge = 3600, origins = "*" , exposedHeaders = "**")
 public class UserController {
 
 	@Autowired
