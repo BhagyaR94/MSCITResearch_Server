72c8e437af7510a53ceaf2855c856cc2ca352994
BhagyaR94 <rathnayake.bhagya94@gmail.com>
2022-07-16 12:44:13 +0530

    hard coded values removed.

--- src/main/java/com/bhagya/research/controller/DestinationController.java
+++ src/main/java/com/bhagya/research/controller/DestinationController.java
@@ -2,10 +2,12 @@ package com.bhagya.research.controller;
 
 import com.bhagya.research.business.destination.dto.DestinationCategory;
 import com.bhagya.research.business.destination.dto.DestinationDTO;
+import com.bhagya.research.business.destination.dto.DestinationRequestDTO;
 import com.bhagya.research.business.destination.service.DestinationService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
@@ -18,8 +20,8 @@ public class DestinationController {
     DestinationService destinationService;
 
     @PostMapping("/destination")
-    public List<DestinationDTO> getDestination() throws JsonProcessingException {
-        return destinationService.loadDestinationsByPreference();
+    public List<DestinationDTO> getDestination(@RequestBody DestinationRequestDTO destinationRequestDTO) throws JsonProcessingException {
+        return destinationService.loadDestinationsByPreference(destinationRequestDTO);
     }
 
     @PostMapping("/loadDestinationCategories")
