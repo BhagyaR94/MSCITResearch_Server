5b6519fe1bc3464448ea3c7630b87011bcaac954
BhagyaR94 <rathnayake.bhagya94@gmail.com>
2022-07-16 13:18:25 +0530

    hard coded values moved into a static class

--- src/main/java/com/bhagya/research/external/ExternalUtils.java
+++ src/main/java/com/bhagya/research/external/ExternalUtils.java
@@ -1,4 +1,8 @@
 package com.bhagya.research.external;
 
 public class ExternalUtils {
+
+//    public static final String ALGO_SERVER = "http://127.0.0.1:5000/";
+    public static final String ALGO_SERVER = "https://research-algorithm.herokuapp.com/";
+
 }
